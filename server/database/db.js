5c9eae925206a9a0d951437dbaa6440ce36a6876
Mansi1527 <mansiy1527@icloud.com>
2024-01-12 17:53:21 +0530

    new bro

--- server/database/db.js
+++ server/database/db.js
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
+import express from "express"
+import dotenv from "dotenv";
+dotenv.config();
+
 const dbconnection= async()=>{
-    const MONODB_URI=`mongodb://mansiy1527:Vijay@ac-rujv8b2-shard-00-00.j2zfgcq.mongodb.net:27017,ac-rujv8b2-shard-00-01.j2zfgcq.mongodb.net:27017,ac-rujv8b2-shard-00-02.j2zfgcq.mongodb.net:27017/?ssl=true&replicaSet=atlas-gew5tu-shard-0&authSource=admin&retryWrites=true&w=majority`;
+    const MONODB_URI=`mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@ac-rujv8b2-shard-00-00.j2zfgcq.mongodb.net:27017,ac-rujv8b2-shard-00-01.j2zfgcq.mongodb.net:27017,ac-rujv8b2-shard-00-02.j2zfgcq.mongodb.net:27017/?ssl=true&replicaSet=atlas-gew5tu-shard-0&authSource=admin&retryWrites=true&w=majority`;
+    // const MONODB_URI=`mongodb+srv://mansiy1527:${process.env.MONGODB_PASSWORD}@filesharing.j2zfgcq.mongodb.net/?retryWrites=true&w=majority`
     try{
         await mongoose.connect(MONODB_URI,{useNewUrlParser:true});
         console.log('database connected successfully');
--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import router from "./routes/routes.js";
 import cors from 'cors';
 import dbconnection from "./database/db.js";
 
-const app=express();//initialize express
+const app=express();
 app.use(cors());
 app.use('/',router);
 const PORT =8000;
