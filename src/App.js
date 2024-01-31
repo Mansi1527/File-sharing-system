982b2908ae829393ecb46b3206b03c0a4be0c7b2
Mansi1527 <mansiy1527@icloud.com>
2024-02-01 04:18:23 +0530

    mw

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import {useState ,useEffect, useRef } from "react";
 import './App.css';
+import {uploadFile} from './service/api';
 
 function App() {
+  const [file,setFile]=useState('');
+  const [result,setResult]=useState('');
+
+  const fileInputRef=useRef();
+
+  
+  
+  //const logo = "https://c1.wallpaperflare.com/preview/511/343/665/lifestyle-objects-people.jpg";
+  const logo ="https://media.istockphoto.com/videos/abstract-background-black-and-white-computer-motherboard-16-video-id1133856296?s=640x640"
+
+  useEffect(()=>{
+    const getImage= async()=>{
+        if(file){
+          const data= new FormData();
+          data.append("name",file.name);
+          data.append("file",file);
+
+          const response=await uploadFile(data);
+          setResult(response.path);
+          
+        }
+    }
+    getImage();
+  } ,[file])
+  const onUploadClick=()=>{
+    fileInputRef.current.click();
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header>
+    <div className="container">
+      <img src={logo} alt="background"/>
+      <div className="textimage">  
+      <h1>file sharing system</h1>
+      <p>Enjoy uploading and sharing across the globe!</p>
+      <button onClick={()=>onUploadClick()}>upload</button>
+      <input type="file" 
+      ref={fileInputRef}
+       style={{display:"none"}}
+       onChange={(e)=> setFile(e.target.files[0])}
+       />
+
+       <a href={result} target="_blank" rel="noreferrer">{result}</a>
+      </div>
+     
+      <div></div>
     </div>
+    </header>
   );
 }
 
 export default App;
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// const React=require("react");
+// const ReactDOM=require("react-dom/client");
+// const App=require("./App");
+// const reportWebVitals=require("./reportWebVitals");
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
